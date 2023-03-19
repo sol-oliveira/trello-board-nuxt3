e48a59d6b8d1e4b62002f7fd53a6d24ad8984469
Sol Oliveira <sole-oliveira@hotmail.com>
2023-03-18 21:55:51 -0300

    style: component TrelloBoard

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -40,8 +40,10 @@ const columns = ref<Column[]>([
 </script>
 
 <template>
-    <div>
-        <div v-for="column in columns" :key="column.id">        
+    <div class="flex gap-4 overflow-x-auto items-start">
+        <div v-for="column in columns" :key="column.id"
+        class="column bg-gray-200 p-5 rounded min-w-[250px]"
+        >        
             <header>
                 {{ column.title }}
             </header>
