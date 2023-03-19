e39c7c120cded7fc9c6508c726437ffd8d4d238c
Sol Oliveira <sole-oliveira@hotmail.com>
2023-03-19 20:50:46 -0300

    add: draggable transition and drag handle

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -46,10 +46,13 @@ const columns = ref<Column[]>([
             v-model="columns"
             group="columns"
             item-key="id"
+            :animation="150"
+            handle=".dra-handle"
             class="flex gap-4 overflow-x-auto items-start">
             <template #item="{element: column} : {element : Column}">
                 <div class="column bg-gray-200 p-5 rounded min-w-[250px]">        
                     <header class="font-bold mb-4">
+                       <DragHandle/>
                         {{ column.title }}
                     </header>
                     <TrelloBoardTask v-for="task in column.tasks" :task="task" />
