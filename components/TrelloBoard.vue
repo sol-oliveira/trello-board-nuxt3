059eb9d307941b7e8edbbfe049732005b2d8e17a
Sol Oliveira <sole-oliveira@hotmail.com>
2023-03-19 20:37:43 -0300

    add: sort the columns with Vue Draggable

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -2,6 +2,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { nanoid } from 'nanoid'
+import draggable from 'vuedraggable'
 import type { Column } from '../types';
 
 const columns = ref<Column[]>([
@@ -40,18 +41,26 @@ const columns = ref<Column[]>([
 </script>
 
 <template>
-    <div class="flex gap-4 overflow-x-auto items-start">
-        <div v-for="column in columns" :key="column.id" class="column bg-gray-200 p-5 rounded min-w-[250px]">        
-            <header class="font-bold mb-4">
-                {{ column.title }}
-            </header>
-            <TrelloBoardTask v-for="task in column.tasks" :task="task" />
-            <footer>
-                <button class="text-gray-500">
-                    + Add a Card
-                </button>
-            </footer>
-        </div>
+    <div >
+        <draggable
+            v-model="columns"
+            group="columns"
+            item-key="id"
+            class="flex gap-4 overflow-x-auto items-start">
+            <template #item="{element: column} : {element : Column}">
+                <div class="column bg-gray-200 p-5 rounded min-w-[250px]">        
+                    <header class="font-bold mb-4">
+                        {{ column.title }}
+                    </header>
+                    <TrelloBoardTask v-for="task in column.tasks" :task="task" />
+                    <footer>
+                        <button class="text-gray-500">
+                            + Add a Card
+                        </button>
+                    </footer>
+                </div>
+            </template>    
+        </draggable>       
     </div>
 </template>
 
