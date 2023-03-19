2ac85720646f7c90b83df08c7213b12b521199a8
Sol Oliveira <sole-oliveira@hotmail.com>
2023-03-18 23:20:21 -0300

    add: Task Component

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -41,15 +41,16 @@ const columns = ref<Column[]>([
 
 <template>
     <div class="flex gap-4 overflow-x-auto items-start">
-        <div v-for="column in columns" :key="column.id"
-        class="column bg-gray-200 p-5 rounded min-w-[250px]"
-        >        
-            <header>
+        <div v-for="column in columns" :key="column.id" class="column bg-gray-200 p-5 rounded min-w-[250px]">        
+            <header class="font-bold mb-4">
                 {{ column.title }}
             </header>
-            <p v-for="task in column.tasks" :key="task.id">
-                {{ task.title }}
-            </p>
+            <TrelloBoardTask v-for="task in column.tasks" :task="task" />
+            <footer>
+                <button class="text-gray-500">
+                    + Add a Card
+                </button>
+            </footer>
         </div>
     </div>
 </template>
---
+++ components/TrelloBoardTask.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { Task } from '@/types';
+
+defineProps<{
+    task: Task
+}>()
+
+</script>
+
+<template>
+    <div :title="task.createdAt.toLocaleDateString()" class="task bg-white p-2 mb-2 rounded shadow-sm max-w-[250px] flex"
+     tabindex="0">      
+      <span>{{ task.title }}</span>
+    </div>
+  </template>
+
+<style scoped>
+
+</style>
