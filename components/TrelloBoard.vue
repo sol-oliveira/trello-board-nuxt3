f541a936c0b9160b7b87e7c4e6d064d82fc4479b
Sol Oliveira <sole-oliveira@hotmail.com>
2023-03-19 22:49:07 -0300

    add: Edit, Add, and Remove Columns

--- components/TrelloBoard.vue
+++ components/TrelloBoard.vue
@@ -39,22 +39,38 @@ const columns = ref<Column[]>([
 
 const alt = useKeyModifier("Alt")
 
+function createColumn() {
+  const column: Column = {
+    id: nanoid(),
+    title: '',
+    tasks: [],
+  }
+  columns.value.push(column)
+  
+  nextTick(() => {
+        (document.querySelector('.column:last-of-type .title-input') as HTMLInputElement).focus()
+    })
+}
+
 </script>
 
 <template>
-    <div >
+    <div class="flex items-start overflow-x-auto gap-4">
         <draggable
             v-model="columns"
             group="columns"
             item-key="id"
             :animation="150"
             handle=".dra-handle"
-            class="flex gap-4 overflow-x-auto items-start">
+            class="flex gap-4 items-start">
             <template #item="{element: column} : {element : Column}">
                 <div class="column bg-gray-200 p-5 rounded min-w-[250px]">        
                     <header class="font-bold mb-4">
                        <DragHandle/>
-                        {{ column.title }}
+                       <input class="title-input bg-transparent focus:bg-white rounded px-1 w-4/5"
+                        @keyup.enter="($event.target as HTMLInputElement).blur()"
+                        @keydown.backspace="column.title === '' ? columns = columns.filter(col => col.id !== column.id) : null"
+                        type="text" v-model="column.title" />
                     </header>
                     <draggable
                         v-model="column.tasks"
@@ -75,7 +91,10 @@ const alt = useKeyModifier("Alt")
                     </footer>
                 </div>
             </template>    
-        </draggable>       
+        </draggable> 
+        <button @click="createColumn" class="bg-gray-200 whitespace-nowrap p-2 rounded opacity-50">
+            + Add another column
+         </button>      
     </div>
 </template>
 
